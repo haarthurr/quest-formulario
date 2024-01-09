ad4fac7f319a65b2e47d597b640d6f9bdc4c35f1
Haarthurr <am9968424@gmail.com>
2024-01-09 10:00:02 -0300

    first commit

---
+++ src/js/validacao-campos.js
@@ -0,0 +1,21 @@
+const inputs = document.querySelectorAll('.input');
+const avisos = document.querySelectorAll('.aviso-obrigatorio')
+const formulario = document.querySelector('.formulario')
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault()
+    inputs.forEach((input, index, array) => {
+        let inputEstaVazio = input.value === ""
+
+        if (inputEstaVazio) {
+            input.classList.remove('borda-verde')
+            input.classList.add('borda-vermelha');
+            avisos[index].classList.remove('esconder')
+        } else {
+            input.classList.remove('borda-vermelha')
+            input.classList.add('borda-verde')
+            avisos[index].classList.add('esconder')
+        }
+
+    })
+})
